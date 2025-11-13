1abb1ef73af0a30c3b6a75ae87ec7e8db9c6af7e
SharenfromHR <danieljerome97@hotmail.com>
2025-11-13 10:46:44 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))"
+      }
+    }
+  },
+  plugins: [require("tailwindcss-animate")]
+};
